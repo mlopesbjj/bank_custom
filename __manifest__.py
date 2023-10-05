7613f114d263ff4133635997ef4e210f27c8ab82
Marcio Lopes <mlopesbjj@gmail.com>
2023-10-05 17:08:55 +0200

    [CREATE]
    Signed-off-by: Marcio Lopes <mlopesbjj@gmail.com>

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "KHPS Custom BANK",
+    "description": "ADD il campo x_acc_number tipo many2one",
+    "summary": "ADD il campo x_acc_number tipo many2one",
+    "author": "Marcio Lopes - KHPS",
+    "depends": ['account'],
+    "data": ['views/respartnerbank.xml']
+}
---
+++ models/ResPartnerBank.py
@@ -0,0 +1,24 @@
+from odoo import models, fields, api
+
+class ResPartnerBank(models.Model):
+    print('executou a parada 1.')
+    _inherit = 'res.partner.bank'
+    x_acc_number = fields.Many2one(
+        'res.partner.bank', 
+        string="N. Banca", 
+        domain="[('acc_number', '!=', False)]" 
+    )
+    
+    acc_number = fields.Char(
+        # string='.',
+        invisible=1,
+        required=1
+    )
+
+    @api.onchange('x_acc_number')
+    def onchange_x_acc_number(self):
+        print('executou a parada 2.')
+        if self.x_acc_number:
+            self.acc_number = self.x_acc_number.acc_number
+        else:
+            self.acc_number = ''
